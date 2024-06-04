5e441400c44ea95dafce083824e8b021471cdb39
Hadia Khalil <hadiyakhalil183@gmail.com>
2024-06-03 21:51:57 -0700

    forth-class.js

---
+++ forth-class.js
@@ -0,0 +1,111 @@
+// let ice = +prompt("inter your num");
+// if(ice === 100){
+//     console.log("icecream is corenetto");
+// }else if(ice === 500){
+//     console.log("icecream is correnetto family pack 1.5 ltr")
+// }else if(ice === 1000){
+//     console.log("icecream is correnetto family pack 2.5 ltr")
+// }else{
+// console.log("your ice cream is random")
+
+
+
+// }
+
+// let ice = +prompt('inter your num');
+    
+// switch (ice) {
+//     case 100:
+//         console.log("icecream is corenetto");
+//         break;
+//         case 500:
+//             console.log("icecream is correnetto family pack 1.5 ltr");
+//             break;
+//             case 1000:
+//                 console.log("icecream is correnetto family pack 2.5 ltr") ;  
+//                 break;
+//         default:
+//             console.log("your ice cream is random");}
+
+
+// let ice = +prompt("what is your num");
+// if(ice === 300){
+//    console.log("icecream is corenetto");
+// }else if(ice === 400){
+//     console.log("icecream is corenetto faily pack 5 ltr") 
+// }else{
+//     console.log("icecream is corenetto faily pack 8 ltr") 
+// }
+
+
+            
+//  let ice = +prompt("your num")
+//  switch (ice) {
+//     case 200:
+//     console.log("ice-cream is corrnetto");
+//     break;
+//     case 300:
+//     console.log("ice-cream is corrnetto pack 1 LtR");
+//     default:
+//         console.log("ice-cream is corrnetto 1.5 ltr");
+//  }
+
+
+
+
+// let username = prompt("even num");
+// let compnum = Math.round(Math.random () * 10);
+
+// if(compnum % 2 === 0 && username === "even"){
+//     console.log("you win !" +compnum  );
+// }else if(compnum % 2 !== 0 && username === "odd"){
+//     console.log("you win !" +compnum  );
+// }else{
+//     console.log("you loss !" +compnum  );
+// }
+
+
+// let username = prompt("even num");
+// let compnum = Math.round(Math.random () *10)
+// if(compnumn % 2 === 0 && username === "even"){
+//    console.log("uh win !" +compnum); 
+// }else if(compnum % 2 ! == 0 && )
+
+
+///.............................................................................................
+// loop
+/// initialization
+///condition
+// ///increment
+// for(let i = 1; i <= 9; i++ ){
+//       console.log("hello" + 1)
+// }
+
+// for(let i = 1; i <= 9; i++ ){
+//    console.log("2 x" + i + "=" + 2 *i);
+// }
+
+
+// for (let i =1; i <= 10; i++){
+//    console.log("2 x " + i + "=" + 2 *i);
+// }
+
+// for(let i = 1; i <= 10; i++ ){
+// console.log("hello world" + i)
+// }
+
+//  for(let i = 1; i <= 10; i++ ){
+//      console.log("2 x" + i + " = " + 2 *i)}
+     
+     
+    //  let table = 6;
+    //  for(let i = 1; i <= 20; i++ ){
+    //     console.log(table +  " x " + i + " = " + table *i)}
+
+
+    // let table = 6;
+    // for(let i = 1; i <= 20; i++ ){
+    //    console.log(table +  " x " + i + " = " + table *i)
+    //    console.log(Math.round(Math.random () * 10));
+    // }
+
