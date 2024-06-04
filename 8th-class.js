85c586930efbbc0dd6002d36d84ad545580fce72
Hadia Khalil <hadiyakhalil183@gmail.com>
2024-06-03 23:50:10 -0700

    8th-class.js

---
+++ 8th-class.js
@@ -0,0 +1,139 @@
+// let person = {
+//     name : "wasif",
+//     cnic : 68675785857,
+//     language : "urdu",
+//     class : "javascript",
+//     speck : function(){
+// console.log();
+//     }
+// }
+// console.log(person);
+
+////////////////////////////////////////////////////////////////////////////////////
+
+// let person1 = {
+//     name : "wasif",
+//     cnic : 68675785857,
+//     language : "urdu",
+//     class : "javascript",
+//     speck : function(student){
+// console.log( student + this.name);
+//     }
+// }
+// let person2 = {
+          
+
+//     name : "ali",
+//     cnic : 68675785857,
+//     language : "urdu",
+//     class : "javascript",
+//     speck : function( student){
+// console.log(student + this.name);
+//     }
+// }
+
+// person1.speck("hello");
+// person2.speck("hello")
+
+
+
+//////////////////////////////////////////////////////////////////////
+// let person1 = {
+//     name : "wasif",
+//     cnic : 68675785857,
+//     language : "urdu",
+//     class : "javascript",
+//     speck : function(student){
+// console.log( student + this.name);
+//     }
+// }
+// let person2 = {
+        
+//     name : "ali", //property
+//     cnic : 68675785857, ///key
+//     language : "urdu",
+//     class : "javascript",
+//     speck : function( student){
+// console.log(student + this.name);
+//     }
+// }
+// let student = [
+//     person1, person2
+// ];
+// console.log(student)
+// //////////////////////////////////////////////////////////////////////////////////////////
+// 
+
+// function student (){
+//    return {
+
+//     name : prompt("inter your name"), 
+//         cnic : prompt("inter your cnic"),  
+//         language :prompt("inter your langu"), 
+//         class : prompt("inter your class"), 
+
+//         }
+// }
+
+// let students = [student( )];
+// console.log(students)
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// java script map method
+
+
+ // let a = [1,2,3,4,5,6,7]
+// b = 10;
+//  let table = a.map((num) => num * b);
+//  console.log(table);
+
+// let a = [2,3,4,5,7]
+//  let b = a.map(function(val,ind) {
+//     console.log(ind,val);
+//     return val * 2
+//  })
+//  console.log(b);
+
+
+
+// let persons = ["ali","ahmad","haroon","zain"];
+// let person2 = persons.map(function(students) {
+//     return '<li> $(students) </li>'
+// })
+// console.log(persons);
+// console.log(person2);
+
+// let nums = [1,2,3,4,5,6,7,8,9,10];
+// let num = 5;
+// let table = nums.map((num1) => num1 * num);
+// console.log(table);
+
+
+// let numbers = [1,2,3,4,5];
+// let num2 = 2;
+// let table  = numbers.map((num) => num * num2);
+// console.log(table);
+
+
+
+// let persons = ["hh" ,"hhd"];
+// let person2 = persons.map(function(name) {
+//     return ' <li> $(name) </li> '
+// })
+// console.log(persons);
+
+
+// let a = ["hadii","hanii","shazii"]
+// let b = a.map(function (c) {
+//     return '<li> $(c)</li>'
+// })
+// console.log(a);
+// console.log(b);
+
+
+// let  numbers = [1,2,3,4,5,6,7,8,9,10];
+// let num = 5;
+// let table = numbers.map((a) => a * 5)
+// console.log(table);
