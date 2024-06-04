2467d61a0b61fef689af09030ab55b78d4cd4ba7
Hadia Khalil <hadiyakhalil183@gmail.com>
2024-06-03 21:50:14 -0700

    third-class.js

---
+++ third-class.js
@@ -0,0 +1,119 @@
+// == ===
+// < > 
+// <= >==
+// != is not  ==! is equall
+
+
+//  == === 
+//  comparision operatrs.....................................
+
+// ==
+// value chck
+
+// ===
+// value type chck
+
+
+// let num = 9;
+// let num2 = 8;
+// console.log(9 > 8)
+
+// let a = 90;
+// let b = 78;
+// console.log(a <= b)
+
+// let age = prompt("inter your age")
+// if (age <= 18){
+//     console.log("you can apply cnic")
+// }else{
+//     console.log("uh are eligble")
+// }
+
+
+// let num1 = 6;
+// let num2 = 6;
+// console.log(num1 == num2);
+// true
+
+
+// let num1 = "6";
+// let num2 = 6;
+// console.log(num1 == num2);
+// true
+
+
+// let num1 = "6";
+// let num2 = 6;
+// console.log(num1 === num2);
+// false
+
+// let num1 = "7";
+// let num2 = 6;
+// console.log(num1 == num2);
+// false
+
+// != ==!
+// not equal to and equal to.....................................................
+// comparision operatrs
+//  let num1 = "6";
+//  let num2 = 6;
+//  console.log(num1 != num2);
+//  not is equal to !=
+
+// console.log("5" !== 5)
+// true
+
+// console.log("5" != 7)
+// // true
+
+
+{/* < greatrthen.............................................
+ > lessthen
+ // comparision operatrs */}
+
+
+//  console.log(5 < 7)
+// true
+
+
+
+//  let num = 66;
+// let num2 = 5;
+//  console.log(num < num2)
+//  false
+
+// <= <== 
+// greatrthn is qual to and greaterthen double is equal to
+
+// let num = 66;
+// let num2 = 5;
+//  console.log(num <= num2)
+//  false
+
+// let num = 66;
+// let num2 = 90;
+//  console.log(num <= num2)
+// true
+
+
+
+// // condition
+// let age =  prompt("inter your age")
+// if(age >= 18){
+//     console.log("you can apply cnic")
+// }else{
+//     console.log("you can not eligble")
+// }
+
+// let username = +prompt("inter your num");
+// let computerNumber = Math.round(Math.random() * 5);
+// if(username === computerNumber){
+//     console.log(" uh win ");
+// }else{
+//     console.log("uh lose !" + computerNumber);
+// }
+
+// prompt 
+// by default  value string 
+// +prompt
+// value change string to num
