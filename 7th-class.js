f21621827c455b3a15da3017740588d2062cbdb4
Hadia Khalil <hadiyakhalil183@gmail.com>
2024-06-03 23:47:42 -0700

    7th-class.js

---
+++ 7th-class.js
@@ -0,0 +1,146 @@
+// for in loop
+// how many index in array
+// for off loop
+//  value in index? index mn value kia h
+/// ..............................................................
+
+// let nam = ["xggg","dgdggd","eddggd","edhud"];
+// for (let elem of nam){
+//     console.log(elem);
+// }
+
+// let arr = ["hadii", "wajeeha", "shazii", "hano", "gull"];
+//  arr.forEach(function(val,ind) {
+//     console.log(ind,val);
+//  } )
+    
+
+
+//  let name = ["hadii", "wajeeha", "shazii", "hano", "gull"];
+//  name.forEach(function(val,ind) {
+    
+//  console.log(ind,val);
+    
+//  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///..........................................................
+////for in loop
+// let name = ["hadii", "wajeeha", "shazii", "hano", "gull"];
+// for (let elem in name){
+//        console.log(elem);
+// }
+
+// for off loop
+// let name = ["hadii", "wajeeha", "shazii", "hano", "gull"];
+//  for (let elem of name){
+//         console.log(elem);
+//  }
+
+
+// let arr = ["hadii","hanii","shazii"];
+// arr.forEach(function(ind, val) {
+//     return val + ind
+// })
+// console.log(ind, val);
+//foreach.................
+// callback function
+// aesa function jo dusry function k andr as a parameter paas hora ho
+// two value
+// value and index
+// let arr = ["hadii", "wajeeha", "shazii", "hano", "gull"];
+//  let a = arr.reduce(function(val,ind) {
+//     return ind + ' ' + val;
+// })
+
+// console.log(a);
+
+// let arr = ["mango", "apple", "orange"];
+// console.log(arr);
+// let arrsort = arr.sort(function(a,b) {
+//     if(a > b){
+//         return 1
+//     }else{
+//         return -1
+//     }
+// })
+// console.log(arrsort);
+
+
+// array filter
+// let filterarr = [1, 2, 3, 4, 5, 6, 7, 8, 9,10];
+// let arr = filterarr.filter(function(val){
+// if(val & 2 !=0){
+//     return true;
+// }else{
+//     return false
+// }
+// })
+// console.log(filterarr);
+//     console.log(arr);
+
+
+
+///sort
+// repeated value remove
+// let arr = [0,0,0,1,5,6,7,4,7,9,3,445,77,99,45,1,6,75,77];
+// let arrsort = arr.sort(function(a,b) {
+//     if(a > b){
+//         return 1
+//     }else{
+//         return -1
+//     }
+// })
+// console.log(arr);
+
+
+
+// array ko a to z krta
+// counting sort
+// let arr = [10,9,11,1,5,6,7,4,7,9,3,445,77,99,45,1,6,75,77];
+// console.log(arr);
+// let arrsort = arr.sort(function(a,b) {
+//     if(a > b){
+//         return 1
+//     }else{
+//         return -1
+//     }
+// })
+// console.log(arrsort);
+
+
+
+//  previous current
+// let arr = [10,9,11,1,5,6,7,4,7,9,3,445,77,99,45,1,6,75,77];
+// let a = arr.reduce(function(preval, currval) {
+//     return preval +  currval;
+// })
+// console.log(a);
+
+//  let arr = ["hadii"," ", "sam"];
+//  let a = arr.reduce(function(preval, currval) {
+//      return preval +  currval;
+//  })
+//  console.log(a);
+
+        
+
+
+
