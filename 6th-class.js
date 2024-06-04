d6b4031092a8c65e1f08fef0f119ef40ae0c8fea
Hadia Khalil <hadiyakhalil183@gmail.com>
2024-06-03 23:45:49 -0700

    6th-class.js

---
+++ 6th-class.js
@@ -0,0 +1,87 @@
+/////array.......it is a container ...we can store multiple data in single variable
+//[]  square bracket
+
+// push................... last add 
+// unshift.................... frst add
+// pop......................... last remove 
+// shift.........................frst remove
+
+
+// let arr = ["hadii","hanii","shazii"];
+//  arr.splice(1,2, "sam" );
+//  console.log(arr);
+
+
+
+// let arr = ["hadia","hania","shazia","wajeeha"];
+// let b = arr.slice(1,4);
+// console.log(b);
+
+// let arr = ["hadii","hania"];
+// arr.push("rafia")
+// console.log(arr);
+
+// let arr = ["1" , "2","3"]
+// arr.shift("")
+// console.log(arr);
+
+
+// let arr = ["shaoib ", "abdullah", "wasif ", "waseem"];
+// let b = arr.slice(1,2)
+// console.log(b);
+
+// let arr = ["hadii", "hadii", "waseem"];
+// arr[0] = "hadii" ///not recomended
+// arr.push("hadi5i") ///array value added in the end
+// console.log(arr);
+
+
+// add variable in first 
+// let arr = ["hadii", "hadii", "waseem"]; 
+// arr.unshift(ali)         // add variable in first 
+// console.log(arr);
+
+// // array value added in the end
+// let arr = ["hadii", "hadii", "waseem"];
+// arr.push(ali)
+// console.log(arr);
+
+// last value remove
+// let arr = ["hadii", "hadii", "waseem"];
+// arr.pop()
+// console.log(arr);
+
+
+//array first value remove
+// let arr = ["hadii", "hadii", "waseem"];
+// arr.shift()
+// console.log(arr);
+
+// let arr = ["hadii", "hadii", "waseem"]; ///array 0 count like 1
+// arr.length
+// console.log(arr);
+
+
+// let arr = ["hadii", "hadii", "waseem"];
+// arr.splice(1, 2, "danish") ///  frst replace secnd delete
+// console.log(arr);
+
+// let arr = ["hadii", "sonii", "waseem"];
+// arr.unshift("ali")
+// arr.splice(1, 2, "danish") ///  frst replace secnd delete
+// console.log(arr);
+
+
+// let a =[0, 1, 2,3,4,5,6,7,8,9];
+// console.log(a);
+
+// let a =[0, 1, 2,3,4,5,6,7,8,9];
+// console.log(a);
+// console.log(a.length);
+
+
+// let arr = ["hadii", "sonii", "waseem"];
+// // arr.splice(2, "danish")
+// let b = arr.slice(0,2) /// 0 is starting point and 3 is ending............
+// console.log(arr);
+// console.log(b);
