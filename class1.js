466bbc73eca9bda264e0b4f38844df5427b955e2
Hadia Khalil <hadiyakhalil183@gmail.com>
2024-06-03 21:44:52 -0700

    firstclass

---
+++ class1.js
@@ -0,0 +1,240 @@
+// let keywrd
+
+// let num = 6;
+// let num2 = 5;
+// console.log(num)
+
+// let data = 5;
+// let data2 = 8;
+// console.log(data + data2)
+
+// const num = 8;
+// console.log(num);
+
+
+// let num = 5;
+// console.log(num);
+// console.log(typeof String(num));
+
+
+//  let name = +prompt("what is your name");
+//  console.log(name +5);
+
+//  == ===
+//  cpmparision operatrs
+
+// ==
+// value chck
+
+// ===
+// value type chck
+
+// let name = prompt("what is ur name");
+// console.log(name);
+// document.write(name);
+
+
+
+// let name = prompt("what is your name");
+// console.log(name);
+// Document.write(name);
+
+
+
+
+// let name = 34;
+// let name2 = true;
+// console.log(typeof name2)
+// console.log(name + name2)
+
+
+
+// let name = prompt("what is this");
+// console.log(name);
+// document.write(name);
+
+// let num = 4444;
+// console.log(num);
+// console.log(typeof String (num))
+
+// let num = 77;
+// let name = true;
+// console.log ( typeof name)
+// console.log(num + name)
+
+
+// let email = "hadiakhalil@gmail.com";
+// let name = "hadia";
+// let account = 56478455757775;
+// let etc
+// console.table([email, name , account , etc]);
+
+
+
+// let name = prompt("what is your name");
+// console.log(name);
+// document.write(name);
+
+// let name = prompt("what is your num.........")
+//  console.log(name);
+
+
+// const num = 9;
+// console.log(num);
+
+// let num = 12;
+// let num2 = 6;
+// console.log(num - num2)
+
+
+// let num = 12;
+// let num2 = 6;
+// console.log(num / num2)
+
+
+
+// let num 
+// console.log(num)
+
+
+// let num = (5+6) + 6 * 12;
+
+// console.log(num)
+
+// let rendomNumber = Math.random();
+// console.log(rendomNumber);
+
+
+// console.log("hello");
+// let num = 77;
+// num =776;
+// num =88;
+// num =33;
+// console.log(num);
+
+//  let firstname = "hwllo";
+//  let secndname = "meee";
+// console.log(firstname + secndname)
+
+// let firstName = "ali"; 
+//  let lastName = "shoaib"; 
+//  console.log(firstName +" " + lastName)
+
+// let num = 22;
+// let num2 = 1;
+// console.log(num + num2);
+
+// // //  const num  = 77;
+// // one time update
+
+// // let num = 44;
+// // let num2 = 66;
+
+// // alert(num + num2)
+// // // num addition
+
+// // let num = "44";
+// // let num2 = 66;
+// // console.log("num + num2")
+// // // result 4466
+// // // number string concatenation
+// // // string number  concatenation
+// //  let firstName = "ali"; 
+// //  let lastName = "shoaib" 
+// //  console.log(firstName + lastName)
+// // //  concatenation
+// // // space
+// // console.log(firstName + " " + lastName)
+
+// let email = "ali@gmail.com";
+// let name = "ahmad";
+// let account = 6637635378736;
+//   let etc
+//   console.table([email , name , account, etc])
+
+
+// console.table([email , name , account, etc])
+
+// // math operators = + _ * / devided % modules
+
+// let num = "77";
+// let num2 = "1";
+// console.log(num + num2)
+// // result 78
+
+// let num = "77";
+// let num2 = "1";
+// console.log(num - num2)
+// // result 76
+
+// let num = "100";
+// let num2 = "5";
+// console.log(num * num2)
+// // result 500
+
+// let num = "100";
+// let num2 = "5";
+// console.log(num / num2)
+// // result 20
+
+// let data = "10";
+// let data2 = "3";
+// let add = data % data2;
+// console.log(add);
+// result 1..........
+
+// let data = "50";
+// let data2 = "4";
+// let add = data % data2;
+// console.log(add);
+// // result 2........
+
+// let name = prompt("what is your name");
+// console.log(name);
+// document.write(name);
+
+// let name = prompt("what is your num.........")
+//  console.log(name);
+
+
+// let rendomNumber = Math.random();
+// console.log(rendomNumber);
+
+// let rendomNumber = Math.random();
+// rendomNumber = rendomNumber * 10;
+// let num = Math.round(rendomNumber);
+// console.log(num);
+
+// let num  = 5 + 5 * 5;
+// console.log(num)
+// result 30.........
+// bodmad.........
+
+// let nu  = (5 + 5) * 5;
+// console.log(num)
+// result 50......
+
+// let x = 33;
+// console.log(x);
+// console.log(typeof String(x));
+
+// let name 
+// console.log(name);
+// result undefined
+// data type....
+
+// let name  = null;
+// console.log(name);
+// result null itx also data type..........\
+// value asign 
+
+
+/////boolean
+// let x = 33;
+// let y = true;
+// console.log(typeof y);
+// console.log(x + y)
+// true value 1..........
+// false value 0..........
+
+// github.com/shoaib420/javascript-classes
